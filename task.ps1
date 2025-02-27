d356e3b7617ac5c6247a4bcc7e981d5403d63c07
88victory88 <victory.otrok@gmail.com>
2025-02-27 11:13:58 +0800

    Solution

--- task.ps1
+++ task.ps1
@@ -20,4 +20,45 @@ $nsgRuleSSH = New-AzNetworkSecurityRuleConfig -Name SSH  -Protocol Tcp -Directio
 $nsgRuleHTTP = New-AzNetworkSecurityRuleConfig -Name HTTP  -Protocol Tcp -Direction Inbound -Priority 1002 -SourceAddressPrefix * -SourcePortRange * -DestinationAddressPrefix * -DestinationPortRange 8080 -Access Allow;
 New-AzNetworkSecurityGroup -Name $networkSecurityGroupName -ResourceGroupName $resourceGroupName -Location $location -SecurityRules $nsgRuleSSH, $nsgRuleHTTP
 
-# ↓↓↓ Write your code here ↓↓↓
+# Creating a virtual network and subnet
+Write-Host "Creating virtual network $virtualNetworkName with subnet $subnetName ..."
+$vnet = New-AzVirtualNetwork -ResourceGroupName $resourceGroupName -Location $location `
+    -Name $virtualNetworkName -AddressPrefix $vnetAddressPrefix
+
+$subnet = Add-AzVirtualNetworkSubnetConfig -Name $subnetName `
+    -VirtualNetwork $vnet -AddressPrefix $subnetAddressPrefix
+
+Set-AzVirtualNetwork -VirtualNetwork $vnet
+
+# Creating a public IP address
+Write-Host "Creating public IP address $publicIpAddressName ..."
+$publicIp = New-AzPublicIpAddress -ResourceGroupName $resourceGroupName -Location $location `
+    -Name $publicIpAddressName -AllocationMethod Static -Sku Basic -DnsName "matebox-dns"
+
+# Creating an SSH key resource
+Write-Host "Creating SSH key resource $sshKeyName ..."
+$sshKey = New-AzSshKey -ResourceGroupName $resourceGroupName -Location $location `
+    -Name $sshKeyName -PublicKey $sshKeyPublicKey
+
+# Retrieving resources to create a network interface
+$vnet = Get-AzVirtualNetwork -ResourceGroupName $resourceGroupName -Name $virtualNetworkName
+$subnet = Get-AzVirtualNetworkSubnetConfig -VirtualNetwork $vnet -Name $subnetName
+$nsg = Get-AzNetworkSecurityGroup -ResourceGroupName $resourceGroupName -Name $networkSecurityGroupName
+$publicIp = Get-AzPublicIpAddress -ResourceGroupName $resourceGroupName -Name $publicIpAddressName
+
+# Creating a network interface for the VM
+Write-Host "Creating network interface for VM ..."
+$nic = New-AzNetworkInterface -ResourceGroupName $resourceGroupName -Location $location `
+    -Name "$vmName-nic" -SubnetId $subnet.Id -PublicIpAddressId $publicIp.Id -NetworkSecurityGroupId $nsg.Id
+
+# Creating a virtual machine
+Write-Host "Creating virtual machine $vmName ..."
+New-AzVm -ResourceGroupName $resourceGroupName -Location $location `
+    -Name $vmName -Size $vmSize -Image $vmImage `
+    -PublicIpAddressName $publicIpAddressName `
+    -VirtualNetworkName $virtualNetworkName -SubnetName $subnetName `
+    -SecurityGroupName $networkSecurityGroupName -SshKeyName $sshKeyName `
+    -OpenPorts 22,8080
+
+Write-Host "Deployment completed successfully."
+
